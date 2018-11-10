ca5e724553f4647b3b452a3d239ce0e23ef475a6
Nicolás Flappo <nicoflappo7@gmail.com>
2018-11-10 15:22:27 -0300

    RoutingModule

--- DatingAPPWeb/src/app/nav/nav.component.ts
+++ DatingAPPWeb/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
- import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
 
@@ -9,7 +10,7 @@ import { AlertifyService } from '../_services/alertify.service';
 })
 export class NavComponent implements OnInit {
   model: any = {};
-  constructor(public authService: AuthService, private alertify: AlertifyService) { }
+  constructor(public authService: AuthService, private alertify: AlertifyService, private router: Router) { }
 
   ngOnInit() {
   }
@@ -20,7 +21,8 @@ export class NavComponent implements OnInit {
     },
     error => {
       this.alertify.error(error);
-    });
+    },
+    () => this.router.navigate(['/members']));
   }
   loggedIn() {
     return this.authService.loggedIn();
@@ -28,5 +30,6 @@ export class NavComponent implements OnInit {
   logout() {
     localStorage.removeItem('token');
     this.alertify.message('logged out');
+    this.router.navigate(['/home']);
   }
 }
